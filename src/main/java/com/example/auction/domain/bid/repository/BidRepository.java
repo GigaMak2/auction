8933fd8bbf46819a817cefede613db47aa86f27c
hhjo <43205471+hhjo96@users.noreply.github.com>
2026-04-09 16:35:34 +0900

    feat: 내입찰조회, 특정경매의 입찰조회 구현

--- src/main/java/com/example/auction/domain/bid/repository/BidRepository.java
+++ src/main/java/com/example/auction/domain/bid/repository/BidRepository.java
@@ -1,10 +1,16 @@
 package com.example.auction.domain.bid.repository;
 
 import com.example.auction.domain.bid.entity.Bid;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface BidRepository extends JpaRepository<Bid, Long>, BidCustomRepository {
     Optional<Bid> findWinnerBidByAuctionId(Long auctionId);
+
+    Page<Bid> findAllByUserId(Long userId, Pageable pageable);
+
+    Page<Bid> findAllByAuctionId(Long auctionId, Pageable pageable);
 }
