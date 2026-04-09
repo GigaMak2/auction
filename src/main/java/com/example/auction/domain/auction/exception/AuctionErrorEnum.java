f983ad98cab7c83d3a0e6e8fb203b4f3d9d4b2ce
imprity <imprity041@gmail.com>
2026-04-09 14:17:14 +0900

    feat: Auction entity 생성
    Auction entity를 생성하고 이를 위한 enum들을 생성

--- src/main/java/com/example/auction/domain/auction/exception/AuctionErrorEnum.java
+++ src/main/java/com/example/auction/domain/auction/exception/AuctionErrorEnum.java
@@ -1,11 +1,11 @@
-package com.example.auction.domain.auction;
+package com.example.auction.domain.auction.exception;
 
 import com.example.auction.common.exception.ErrorEnumInterface;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
- public enum AuctionErrorEnum implements ErrorEnumInterface {
+public enum AuctionErrorEnum implements ErrorEnumInterface {
 
     // 경매 관련 에러
      INVALID_MINIMUM_AUCTION(HttpStatus.BAD_REQUEST, "경매를 찾을 수 없습니다");
