ca414971e1a3fe679f0e48e3a2ad458cf5e0ff13
Soomin <soomin02094@gmail.com>
2026-04-10 13:10:13 +0900

    feat: 마이페이지 조회 API 구현

--- src/main/java/com/example/auction/domain/user/controller/UserController.java
+++ src/main/java/com/example/auction/domain/user/controller/UserController.java
@@ -1,7 +1,14 @@
 package com.example.auction.domain.user.controller;
 
+import com.example.auction.common.config.security.CustomUserDetails;
+import com.example.auction.common.dto.BaseResponse;
+import com.example.auction.domain.user.dto.UserGetResponse;
 import com.example.auction.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<BaseResponse<UserGetResponse>> myPage(
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        Long userId = userDetails.getUserId();
+        return ResponseEntity.status(HttpStatus.OK).body(BaseResponse.success(
+                HttpStatus.OK.name(), "마이페이지 조회 요청 성공", userService.myPage(userId)));
+    }
 }
--- src/main/java/com/example/auction/domain/user/service/UserService.java
+++ src/main/java/com/example/auction/domain/user/service/UserService.java
@@ -1,12 +1,30 @@
 package com.example.auction.domain.user.service;
 
+import com.example.auction.common.exception.ServiceErrorException;
+import com.example.auction.domain.user.dto.UserGetResponse;
+import com.example.auction.domain.user.entity.User;
+import com.example.auction.domain.user.exception.UserErrorEnum;
 import com.example.auction.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public UserGetResponse myPage(Long userId) {
+        User user = userRepository.findByIdAndDeletedFalse(userId).orElseThrow(
+                () -> new ServiceErrorException(UserErrorEnum.USER_NOT_FOUND));
+
+        return new UserGetResponse(
+                user.getId(),
+                user.getEmail(),
+                user.getRating(),
+                user.getRole()
+        );
+    }
 }
