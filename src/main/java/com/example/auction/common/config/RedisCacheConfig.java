eb56888b676893325dac7aff08fa41e5086b7954
Soomin <soomin02094@gmail.com>
2026-04-17 17:49:03 +0900

    feat: 카테고리 목록 조회 Redis 캐싱 적용

--- src/main/java/com/example/auction/common/config/RedisCacheConfig.java
+++ src/main/java/com/example/auction/common/config/RedisCacheConfig.java
@@ -46,6 +46,8 @@ public class RedisCacheConfig {
 
         cacheConfigs.put("getAuction", defaultConfig.entryTtl(Duration.ofMinutes(10)));
 
+        cacheConfigs.put("getCategoryList", defaultConfig.entryTtl(Duration.ofHours(1)));
+
         return RedisCacheManager.builder(connectionFactory)
                 .cacheDefaults(defaultConfig)
                 .withInitialCacheConfigurations(cacheConfigs)
