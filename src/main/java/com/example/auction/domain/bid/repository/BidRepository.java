5c9c64b0a453ba52b067d4663293b1098279672a
hhjo <43205471+hhjo96@users.noreply.github.com>
2026-04-23 12:19:23 +0900

    chore: 안쓰는 임포트 삭제

--- src/main/java/com/example/auction/domain/bid/repository/BidRepository.java
+++ src/main/java/com/example/auction/domain/bid/repository/BidRepository.java
@@ -5,12 +5,6 @@ import com.example.auction.domain.bid.enums.BidAuctionStatus;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Optional;
 
 public interface BidRepository extends JpaRepository<Bid, Long>, BidCustomRepository {
 
