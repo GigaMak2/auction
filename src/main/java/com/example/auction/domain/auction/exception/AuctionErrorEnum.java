64bd913b349e692bd37e2eeeb82e70d749382dfd
hhjo <43205471+hhjo96@users.noreply.github.com>
2026-04-28 11:58:34 +0900

    fix: objectMapper 수정, 람다 함수 비교할 수 있도록 수정

--- src/main/java/com/example/auction/domain/auction/exception/AuctionErrorEnum.java
+++ src/main/java/com/example/auction/domain/auction/exception/AuctionErrorEnum.java
@@ -56,15 +56,7 @@ public enum AuctionErrorEnum implements ErrorEnumInterface {
     AUCTION_CREATE_STARTED_AT_TOO_SOON(
             HttpStatus.BAD_REQUEST,
             "경매 시작 시간은 현재 시간으로부터 최소 10분 이후여야 합니다"
-    ),
-
-    // 직렬화에 실패하는 건 서버 코드 문제
-    AUCTION_SCHEDULE_SERIALIZATION_FAILED(
-            HttpStatus.INTERNAL_SERVER_ERROR,
-            "경매 시간 직렬화에 실패하였습니다"
-
     )
-
 
     ;
 
