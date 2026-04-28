b8ff7546302fdff6b045d5608d805a90c3d698d6
Soomin <soomin02094@gmail.com>
2026-04-28 14:54:55 +0900

    feat: 관리자 전용 유저 상세 조회 API 구현

--- src/main/java/com/example/auction/domain/user/service/UserAdminService.java
+++ src/main/java/com/example/auction/domain/user/service/UserAdminService.java
@@ -1,9 +1,16 @@
 package com.example.auction.domain.user.service;
 
 import com.example.auction.common.dto.PageResponse;
+import com.example.auction.common.exception.ServiceErrorException;
+import com.example.auction.domain.user.dto.UserDetailGetResponse;
 import com.example.auction.domain.user.dto.UserListGetResponse;
 import com.example.auction.domain.user.dto.UserSearchCondition;
+import com.example.auction.domain.user.entity.User;
+import com.example.auction.domain.user.entity.UserSocialAccount;
+import com.example.auction.domain.user.enums.AuthProvider;
+import com.example.auction.domain.user.exception.UserErrorEnum;
 import com.example.auction.domain.user.repository.UserRepository;
+import com.example.auction.domain.user.repository.UserSocialAccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -15,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserAdminService {
 
     private final UserRepository userRepository;
+    private final UserSocialAccountRepository userSocialAccountRepository;
 
     @Transactional(readOnly = true)
     public PageResponse<UserListGetResponse> getUserList(UserSearchCondition condition) {
@@ -26,5 +34,26 @@ public class UserAdminService {
         );
 
         return PageResponse.create(userList);
+    }
+
+    @Transactional(readOnly = true)
+    public UserDetailGetResponse getUserDetail(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new ServiceErrorException(UserErrorEnum.USER_NOT_FOUND));
+
+        AuthProvider authProvider = userSocialAccountRepository.findByUserId(userId)
+                .map(UserSocialAccount::getProvider)
+                .orElse(null);
+
+        return new UserDetailGetResponse(
+                user.getId(),
+                user.getEmail(),
+                user.getRating(),
+                user.getRole(),
+                authProvider,
+                user.isDeleted(),
+                user.getCreatedAt(),
+                user.getDeletedAt()
+        );
     }
 }
