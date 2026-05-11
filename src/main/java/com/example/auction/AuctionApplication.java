0c2a96d4f670eba53b028e583db5c262733baf77
hhjo <43205471+hhjo96@users.noreply.github.com>
2026-05-11 19:30:34 +0900

    feat: 컨피그 및 shedlock enable 추가

--- src/main/java/com/example/auction/AuctionApplication.java
+++ src/main/java/com/example/auction/AuctionApplication.java
@@ -1,11 +1,13 @@
 package com.example.auction;
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableSchedulerLock(defaultLockAtMostFor = "PT30S")
 public class AuctionApplication {
 
     public static void main(String[] args) {
