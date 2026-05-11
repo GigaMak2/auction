8c146d9586e1801f31518fd316c6b583a2c98532
hhjo <43205471+hhjo96@users.noreply.github.com>
2026-05-11 21:27:53 +0900

    fix: 에러 처리 수정

---
+++ src/main/java/com/example/auction/domain/auction/eventBridge/exception/OutboxErrorEnum.java
@@ -0,0 +1,24 @@
+package com.example.auction.domain.auction.eventBridge.exception;
+
+import com.example.auction.common.exception.ErrorEnumInterface;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum OutboxErrorEnum implements ErrorEnumInterface {
+
+    OUTBOX_NOT_FOUND(
+            HttpStatus.NOT_FOUND,
+            "해당하는 이벤트를 찾을 수 없습니다"
+    )
+
+    ;
+
+    private final HttpStatus status;
+    private final String message;
+
+    OutboxErrorEnum(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
